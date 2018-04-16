2d6974ef068998d1b311166e0c0d2b6047136d73
Wei-Wei Wu <wuxx1045@umn.edu>
2018-04-16 00:28:50 -0700

    factoring out DATA

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './index.css';
 import Resume from './Resume';
+import { DATA } from './YOUR_DATA';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Resume />, document.getElementById('root'));
+ReactDOM.render(<Resume data={DATA} />, document.getElementById('root'));
 registerServiceWorker();
